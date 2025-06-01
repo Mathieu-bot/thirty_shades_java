859f98b3063e61c7e94172b42f60e3e0acdd4dfa
Mathieu <hei.i.tafita.2@gmail.com>
2025-06-01 07:29:40 +0300

    feat: add alternate methode in string manipilation

--- StringManipulation.java
+++ StringManipulation.java
@@ -3,55 +3,66 @@ import java.util.List;
 
 public class StringManipulation {
 
-    static int countDigits(String s) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                count++;
-            }
-        }
-        return count;
-    }
+	static int countDigits(String s) {
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				count++;
+			}
+		}
+		return count;
+	}
 
-    static String removeSpace(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (!Character.isWhitespace(s.charAt(i))) {
-                sb.append(s.charAt(i));
-            }
-        }
-        return sb.toString();
-    }
+	static String removeSpace(String s) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			if (!Character.isWhitespace(s.charAt(i))) {
+				sb.append(s.charAt(i));
+			}
+		}
+		return sb.toString();
+	}
 
-    static String slice(String s, int start, int end) {
-        if (start > end) {
-            return "";
-        }
-        StringBuilder sb = new StringBuilder();
-        try {
-            for (int i = start; i < end; i++) {
-                sb.append(s.charAt(i));
-            }
-        } catch (Exception e) {
-            e.printStackTrace(
-                    System.out
-            );
-        }
-        return sb.toString();
-    }
+	static String slice(String s, int start, int end) {
+		if (start > end) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		try {
+			for (int i = start; i < end; i++) {
+				sb.append(s.charAt(i));
+			}
+		} catch (Exception e) {
+			e.printStackTrace(
+				System.out
+			);
+		}
+		return sb.toString();
+	}
 
-    static String reverseWord(String s) {
-        List<String> result = new ArrayList<String>();
-        String[] words = s.split(" ");
-        for (String word : words) {
-            StringBuilder sb = new StringBuilder(word);
-            sb.reverse();
-            result.add(sb.toString());
-        }
-        return String.join(" ", result);
-    }
+	static String reverseWord(String s) {
+		List<String> result = new ArrayList<String>();
+		String[] words = s.split(" ");
+		for (String word : words) {
+			StringBuilder sb = new StringBuilder(word);
+			sb.reverse();
+			result.add(sb.toString());
+		}
+		return String.join(" ", result);
+	}
 
-    public static void main(String[] args) {
-        System.out.println(slice("abc def ghi", 0, 3));
-    }
+	static String alternate(String s, String s1) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			sb.append(s.charAt(i));
+			sb.append(s1.charAt(i));
+		}
+		return sb.toString();
+	}
+
+
+
+	public static void main(String[] args) {
+		System.out.println(alternate("jri", "adn"));
+	}
 }
